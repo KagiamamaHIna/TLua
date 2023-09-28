bfe50538c0232c8d8ff704cf4f63bef9430b5d26
KagiyamaHIna <kagiyamahino@gmail.com>
2023-09-29 07:12:45 +0800

    Lua_dofile_once路径有问题

--- TLua/TLuaCore.cs
+++ TLua/TLuaCore.cs
@@ -137,7 +137,7 @@ namespace TLua
             }
             public static void Lua_dofile_once(string FilePath)
             {
-                string LuaBuf = File.ReadAllText(ExePath + "\\mods\\" + FilePath);
+                string LuaBuf = File.ReadAllText(ExePath + "\\TLua\\" + FilePath);
                 foreach (string str in DoFileName)
                 {//先判断是不是已有的
                     if (str == FilePath)
